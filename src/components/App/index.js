5aa2b6f09fd796f150f47fd26907381de7818b6f
Richard Lock <lock.j.h@gmail.com>
2019-07-02 21:18:51 -0400

    created stats component and copied needed styles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,37 +2,40 @@ import React from 'react';
 import { TopBar, Main, SkipLink, MenuItem, Layout, Icon } from 'cauldron-react';
 import logo from '../../img/icons/logo.svg';
 import './index.css';
+import Stats from '../Stats';
 
-const App = () => (
-  <div className="App">
-    <SkipLink target={'#main-content'} />
-    <TopBar role="banner">
-      <MenuItem>
-        <img alt="" role="presentation" src={logo} />
-        <span>awesome recipes</span>
-      </MenuItem>
-    </TopBar>
-    <Layout>
-      <Main id="main-content" aria-labelledby="main-heading" tabIndex={-1}>
-        <div className="App__head">
-          <div className="confined">
-            <h1 id="main-heading">Recipe Dashboard</h1>
+const App = props =>
+  console.log(props) || (
+    <div className="App">
+      <SkipLink target={'#main-content'} />
+      <TopBar role="banner">
+        <MenuItem>
+          <img alt="" role="presentation" src={logo} />
+          <span>awesome recipes</span>
+        </MenuItem>
+      </TopBar>
+      <Layout>
+        <Main id="main-content" aria-labelledby="main-heading" tabIndex={-1}>
+          <div className="App__head">
+            <div className="confined">
+              <h1 id="main-heading">Recipe Dashboard</h1>
+            </div>
+            <Stats />
           </div>
-        </div>
-        <button
-          type="button"
-          className="Edit"
-          aria-label="Edit {recipe}"
-          onClick={() =>
-            alert('TODO: Build the accessible edit recipe modal! Good luck!')
-          }
-        >
-          <Icon type="fa-pencil" />
-        </button>
-      </Main>
-    </Layout>
-  </div>
-);
+          <button
+            type="button"
+            className="Edit"
+            aria-label="Edit {recipe}"
+            onClick={() =>
+              alert('TODO: Build the accessible edit recipe modal! Good luck!')
+            }
+          >
+            <Icon type="fa-pencil" />
+          </button>
+        </Main>
+      </Layout>
+    </div>
+  );
 
 App.displayName = 'App';
 export default App;
