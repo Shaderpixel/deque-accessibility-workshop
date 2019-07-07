8c71f83df7d51a3836b652f684dcd9347dfa5217
Richard Lock <lock.j.h@gmail.com>
2019-07-07 18:13:49 -0400

    add deque unit tests solution for future reference

--- src/components/Stats/index.test.js
+++ src/components/Stats/index.test.js
@@ -7,10 +7,11 @@ import { fakeStats } from '../../../test-utils/testUtils';
 const statsWrapper = shallow(<Stats stats={fakeStats} />);
 
 describe('<Stats/>', () => {
-  it('have three images', () => {
+  it('have three icons', () => {
     // const wrapper = shallow(<Stats stats={fakeStats} />);
     expect(statsWrapper.find('.Stat__value-icon')).toHaveLength(3);
   });
+
   it('marks each icon as decorative or have a null alt', () => {
     // const wrapper = shallow(<Stats stats={fakeStats} />);
     const presentationalImages = statsWrapper
@@ -30,4 +31,20 @@ describe('<Stats/>', () => {
       expect(node.prop('aria-atomic')).toEqual('true');
     });
   });
+
+  /**
+   * Deque Solutions
+   */
+  // test('marks each icon as decorative', () => {
+  //   statsWrapper.find('.Stat__value img').forEach(icon => {
+  //     expect(icon.is('[role="presentation"]')).toBeTruthy();
+  //     expect(icon.is('[alt=""]')).toBeTruthy();
+  //   });
+  // });
+
+  // test('wraps each stat in a live region', () => {
+  //   expect(statsWrapper.find('[aria-live][aria-relevant][aria-atomic]').length).toBe(
+  //     statsStub.length
+  //   );
+  // });
 });
