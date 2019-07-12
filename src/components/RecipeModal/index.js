a7cc67ee940690fe478de40bb9f5aca6973131d9
Richard Lock <lock.j.h@gmail.com>
2019-07-11 21:03:45 -0400

    Set up RecipeModal container and component

---
+++ src/components/RecipeModal/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button,
+  Modal,
+  ModalContent,
+  TextField,
+  Checkbox
+} from 'cauldron-react';
+import './index.css';
+
+export default class RecipeModal extends Component {
+  render() {
+    return <div></div>;
+  }
+}
